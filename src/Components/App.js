c7543f6511d684c5dca29f15fe63478541a81c5d
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-08-25 23:03:25 +0300

    Добавляет решение ДЗ

--- src/Components/App.js
+++ src/Components/App.js
@@ -12,10 +12,10 @@ import Notification from './Notification';
 export class App extends Component {
   static defaultProps = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -24,6 +24,22 @@ export class App extends Component {
     contacts: this.props.contacts,
     filter: this.props.filter,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   submitFormHandler = ({ name, number }) => {
     const idContact = uuidv4();
